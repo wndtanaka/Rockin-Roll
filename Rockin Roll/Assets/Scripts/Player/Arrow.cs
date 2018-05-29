05ca7866d36674523637a81bf0f0c84812ca2e0a
Duane Reymond <duane.reymond@gmail.com>
2018-05-29 16:44:46 +1000

    Added Arrow script to indicate to Player where current Movement will be.

---
+++ Rockin Roll/Assets/Scripts/Player/Arrow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    public int[] angle;
+    public int currentAngle;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentAngle = 0;
+
+        transform.rotation = Quaternion.Euler(0, angle[currentAngle], 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            if (currentAngle == 3)
+            {
+                currentAngle = 0;
+            }
+            else
+            {
+                currentAngle = currentAngle + 1;
+            }
+
+            transform.rotation = Quaternion.Euler(0,angle[currentAngle],0);
+        }
+    }
+}
