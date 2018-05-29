989e020edb9fb5cb615c34f13ba497b359ef1803
Duane Reymond <duane.reymond@gmail.com>
2018-05-29 11:49:41 +1000

    Added Enemy script and Materials.

---
+++ Rockin Roll/Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform startPos, goTo;
+
+    public float moveSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        startPos = gameObject.transform;
+
+        moveSpeed = 4f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * (Time.deltaTime * moveSpeed));
+        transform.Translate(Vector3.right * (Time.deltaTime * moveSpeed/2));
+    }
+}
