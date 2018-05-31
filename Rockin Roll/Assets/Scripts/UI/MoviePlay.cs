4dd1e39543545f7f3c7c792e1d8b2723a4e7af63
Duane Reymond <duane.reymond@gmail.com>
2018-05-31 23:59:54 +1000

    Made movie loop in MoviePlay script.

--- Rockin Roll/Assets/Scripts/UI/MoviePlay.cs
+++ Rockin Roll/Assets/Scripts/UI/MoviePlay.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoviePlay : MonoBehaviour {
+public class MoviePlay : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         ((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+        ((MovieTexture)GetComponent<Renderer>().material.mainTexture).loop = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
