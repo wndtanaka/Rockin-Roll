7b70d354a18a1da8bb759b584c77d98e88381188
Duane Reymond <duane.reymond@gmail.com>
2018-06-02 13:29:59 +1000

    Minor changes in Score script.

--- Rockin Roll/Assets/Scripts/Player/Score.cs
+++ Rockin Roll/Assets/Scripts/Player/Score.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+using UnityEngine.SceneManagement;
+
 public class Score : MonoBehaviour
 {
     // Actual Score Calculator
--- Rockin Roll/Assets/Scripts/UI/MainMenu.cs
+++ Rockin Roll/Assets/Scripts/UI/MainMenu.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
-{
+
+{ 
     public void PlayButton()
     {
         SceneManager.LoadScene("GameScene");
