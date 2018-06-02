ac0f68aa1668deb7a5dc65800e7ef94ada5c3d31
Unknown <wndtanaka@gmail.com>
2018-06-02 22:35:19 +1000

    Small change for tuts

---
+++ Rockin Roll/Assets/Scripts/Utility/Dummies/DummyDisplay.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyDisplay : MonoBehaviour
+{
+    public GameObject[] enemiesPrefab;
+
+    float timer;
+
+    private void Start()
+    {
+        enemiesPrefab[0].SetActive(true);
+        StartCoroutine(ChangeDisplay(0));
+    }
+
+    IEnumerator ChangeDisplay(int i)
+    {
+        yield return new WaitForSeconds(2);
+        enemiesPrefab[i].SetActive(false);
+        enemiesPrefab[i + 1].SetActive(true);
+        yield return new WaitForSeconds(2);
+        enemiesPrefab[i + 1].SetActive(false);
+        enemiesPrefab[i + 2].SetActive(true);
+        yield return new WaitForSeconds(2);
+        enemiesPrefab[i + 2].SetActive(false);
+        enemiesPrefab[i + 3].SetActive(true);
+        yield return new WaitForSeconds(2);
+        enemiesPrefab[i + 3].SetActive(false);
+        enemiesPrefab[i].SetActive(true);
+        StartCoroutine(ChangeDisplay(i));
+    }
+}
